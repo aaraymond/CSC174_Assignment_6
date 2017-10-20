4e5cb1d932df11132f9a686847a776405e333827
aaraymond <aaraymond@me.com>
2017-10-20 13:14:44 -0400

    created homepage and added bootstrap
    created a home page and added bootstrap files to server

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <!-- Required meta tags -->
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	    <!-- Bootstrap CSS -->
+	    <link type="text/CSS" rel="stylesheet" href="css/bootstrap.min.css">
+	    <!-- Override CSS -->
+	    <link type="text/CSS" rel="stylesheet" href="css/override.css">
+	    <!-- Styles CSS -->
+	    <link type="text/CSS" rel="stylesheet" href="css/styles.css">
+	</head>
+
+    <body>
+    	
+
+    	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </body>
+</html>
