03554967940919277cee3dce86ceb8ad5e514e60
aaraymond <aaraymond@me.com>
2017-10-24 15:12:58 -0400

    structure with filler content
    revised structure and removed bootstrap. added demo content from class

--- index.php
+++ index.php
@@ -3,19 +3,101 @@
 	<head>
 	    <!-- Required meta tags -->
 	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	    <!-- Bootstrap CSS -->
-	    <link type="text/CSS" rel="stylesheet" href="css/bootstrap.min.css">
-	    <!-- Override CSS -->
-	    <link type="text/CSS" rel="stylesheet" href="css/override.css">
-	    <!-- Styles CSS -->
-	    <link type="text/CSS" rel="stylesheet" href="css/styles.css">
+	    <title>Resume - Aaron Raymond</title>
+	    <link rel="stylesheet" href="css/long-scrolly.css">
+	    <link rel="stylesheet" href="css/stylesheet.css">
 	</head>
 
     <body>
-    	
 
-    	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	    <!-- Navigation -->
+	    <nav class="main-menu">
+	        <ul>
+	        	<li><a href="#me">Aaron Raymond</a></li>
+	            <li><a href="#first">Work Experience</a></li>
+	            <li><a href="#second">Skills</a></li>
+	            <li><a href="#third">Leadership</a></li>
+	            <li><a href="#forth">Education</a></li>
+	        </ul>
+	    </nav>
+
+
+	<section id="me">
+		<div class="container">
+			
+			<h1>My Resume</h1>
+
+		</div>
+	</section>
+
+	<!-- First Section -->
+    <section id="first">
+        <div class="container">
+
+            <h1 class="title">Work Experience</h1>
+
+            <div class="content">
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div><!-- .content -->
+
+
+       </div><!-- .container -->
+
+    </section>
+
+    <!-- Second Section -->
+    <section id="second">
+        <div class="container">
+
+            <h1>Skills</h1>
+
+            <div class="content">
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div><!-- .content -->
+
+       </div><!-- .container -->
+
+    </section>
+
+    <!-- Third Section -->
+    <section id="third">
+        <div class="container">
+
+            <h1>Leadership</h1>
+
+            <div class="content">
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div><!-- .content -->
+
+       </div><!-- .container -->
+
+    </section>
+
+    <!-- Forth Section -->
+    <section id="forth">
+        <div class="container">
+
+            <h1>Education</h1>
+
+            <div class="content">
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div><!-- .content -->
+
+       </div><!-- .container -->
+
+    </section>    	
+
+
+
+		<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+    	<script type="text/javascript" src="js/smoothscroll.js"></script>
     </body>
 </html>
